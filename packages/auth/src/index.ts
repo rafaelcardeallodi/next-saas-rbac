4a26ed95ecd5ec27e2f06a04582b5f9d795897c8
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2024-04-14 09:39:38 -0300

    Criando tipagem das subjects

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,20 +2,13 @@ import {
   AbilityBuilder,
   CreateAbility,
   createMongoAbility,
-  ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
+import { ProjectSubject } from './subjects/project'
+import { UserSubject } from './subjects/user'
 
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
