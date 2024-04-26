39fc29730b96de59b1fa8cff98b8141187574fdf
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2024-04-26 09:41:26 -0300

    Rota: Convites pendentes

---
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -0,0 +1,88 @@
+import { roleSchema } from '@acl/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getPendingInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  email: z.string().email(),
+                  role: roleSchema,
+                  createdAt: z.date(),
+                  invite: z.object({
+                    organization: z.object({
+                      name: z.string(),
+                    }),
+                    author: z
+                      .object({
+                        id: z.string().uuid(),
+                        name: z.string().nullable(),
+                        avatarUrl: z.string().nullable(),
+                      })
+                      .nullable(),
+                  }),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        const invites = await prisma.invite.findMany({
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+            organization: {
+              select: {
+                name: true,
+              },
+            },
+          },
+          where: {
+            email: user.email,
+          },
+        })
+
+        return { invites }
+      },
+    )
+}
