93905258fa6b1eae9d095ccee6af320057b13e85
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-06-10 22:38:39 -0300

    Realizando login do usuário

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,6 +1,7 @@
 'use server'
 
 import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
 import { z } from 'zod'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
@@ -28,6 +29,11 @@ export async function signInWithEmailAndPassword(data: FormData) {
       email,
       password,
     })
+
+    cookies().set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
@@ -39,8 +45,6 @@ export async function signInWithEmailAndPassword(data: FormData) {
       }
     }
 
-    console.error(err)
-
     return {
       success: false,
       message:
@@ -49,5 +53,5 @@ export async function signInWithEmailAndPassword(data: FormData) {
     }
   }
 
-  return { success: false, message: null, errors: null }
+  return { success: true, message: null, errors: null }
 }
