f2eff56a26f7f3d8ecb3ee99f85e8aeaa2d18c9f
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-06-11 23:06:42 -0300

    Componente: organization-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,6 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
-import rocketseatIcon from '../assets/rocketseat-icon.svg'
+import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -14,6 +17,10 @@ export function Header() {
           quality={100}
           priority
         />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
       <div className="flex items-center gap-4">
         <ProfileButton />
