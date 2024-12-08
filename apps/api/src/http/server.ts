4be8ed8ac6707eff555d96c554e2c8b519ad0753
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-08 19:49:22 -0300

    chore: use default PORT environment variable

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -114,6 +114,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.SERVER_PORT }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running at http://localhost:3333')
 })
